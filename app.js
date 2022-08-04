39e7650083f946bcb28a97a84ef73c0819226305
Gianfranco Policari <103081719+GianPolicari@users.noreply.github.com>
2022-08-04 18:22:24 -0300

    fs

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
-const express = require('express');
+const express = require('express'); //Requiero a express
+const { dirname } = require('path');
+const path = require('path');//Unifico la rutas
 
 const app = express();
 
+const publicPath = path.resolve(__dirname, './Public');
+app.use( express.static (publicPath) ); // Hace que la carpeta public sea estatica
+
 app.listen(3000, () => console.log('Esto fue muy exitoso!!'));
 
-app.get('/', function(req,res){
-    res.send('Html')
-})
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './Public/views/index.html'));
+
+});
+
+app.get('/registro', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './Public/views/registro.html'));
+
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './Public/views/login.html'));
+
+});
+
+app.get('/contacto', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './Public/views/contacto.html'));
+
+});
+
+app.get('/aboutUs', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './Public/views/aboutUs.html'));
+
+});
